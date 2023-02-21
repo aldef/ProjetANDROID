dded3b9406953101167bf67d4f7aaefa202518e2
alexandredef <alexandre.defrene@gmx.fr>
2023-02-21 15:17:40 +0100

    Ai débuté l'internationalisation et les activities

---
+++ app/src/main/java/fr/upjv/projetandroid/CreatePojoActivity.java
@@ -0,0 +1,14 @@
+package fr.upjv.projetandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePojoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_pojo);
+    }
+}
